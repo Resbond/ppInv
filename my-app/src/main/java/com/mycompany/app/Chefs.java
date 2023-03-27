f0572572cea6d00430b319cf67ffdde4b00b3338
Pepe <jose_ascencioe@hotmail.com>
2023-03-27 18:07:07 -0500

    primeras variables

---
+++ my-app/src/main/java/com/mycompany/app/Chefs.java
@@ -0,0 +1,27 @@
+package com.mycompany.app;
+
+public class Chefs {
+    public static void main(String[] args) {
+        
+        User chefJean = new User();
+        chefJean.name = "Jean Baptise";
+        chefJean.access = 1; 
+        
+        User chefJerry = new User();
+        chefJerry.name = "Jerry Lee";
+        chefJerry.access = 2;
+    
+        User chefLuis = new User();
+        chefLuis.name = "Luis Campos";
+        chefLuis.access = 3;
+
+        User chefKaty = new User();
+        chefKaty.name = "Katy Rios";
+        chefKaty.access = 5;
+
+        User chela = new User();
+        chela.name = "Graciela Diaz";
+        chela.access = 4;
+
+    }
+}
---
+++ my-app/src/main/java/com/mycompany/app/User.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class User {
+    String name;
+    int access;
+    /*Numeros de acceso
+     * 1 = HC (hot kitchen)
+     * 2 = CC (cold kitchen)
+     * 3 = BQT (banquet)
+     * 4 = Emp (employee's lunch)
+     * 5 = Past (pastry)
+     */
+}
